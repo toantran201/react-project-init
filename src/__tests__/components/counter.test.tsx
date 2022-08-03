89b8a5da6d492a2ebb2e146c02756d952a52b68e
toantran201 <toantran201.w@gmail.com>
2022-08-03 16:08:39 +0700

    Add testing library with vitest

---
+++ src/__tests__/components/counter.test.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Counter } from '@/components'
+import { render, screen, userEvent } from '@/utils/test.utils'
+
+describe('Test counter action', () => {
+  test('Counter increment click', () => {
+    render(<Counter />)
+    const btnIncrement = screen.getByRole('button', { name: /increment/i })
+    const message = screen.getByText(/current count:/i)
+    expect(message.textContent).toBe('Current count: 0')
+    userEvent.click(btnIncrement)
+    expect(message).toHaveTextContent('Current count: 1')
+  })
+
+  test('Counter decrement click', () => {
+    render(<Counter />)
+    const btnIncrement = screen.getByRole('button', { name: /decrement/i })
+    const message = screen.getByText(/current count:/i)
+    expect(message.textContent).toBe('Current count: 0')
+    userEvent.click(btnIncrement)
+    expect(message).toHaveTextContent('Current count: -1')
+  })
+})
+
+export {}
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import { fileURLToPath } from 'url'
 import react from '@vitejs/plugin-react'
@@ -9,5 +10,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
-  }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.ts',
+  },
 })
