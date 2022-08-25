a5a980737fbbc6147240933a90504a464a1057ff
toantran201 <toantran201.w@gmail.com>
2022-08-25 10:30:01 +0700

    Test husky with change

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react'
 
 const Counter = () => {
   const [count, setCount] = React.useState(0)
-  const increment = () => setCount(c => c + 1)
-  const decrement = () => setCount(c => c - 1)
-  
+  const increment = () => setCount((c) => c + 1)
+  const decrement = () => setCount((c) => c - 1)
+
   return (
     <div>
       <div>Current count: {count}</div>
