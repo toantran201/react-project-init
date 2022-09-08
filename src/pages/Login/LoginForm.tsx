d0be883c2d390584690f2b70f5152f6b7612f618
toantran201 <toantran201.w@gmail.com>
2022-09-08 15:18:09 +0700

    Update lint and prettier

--- src/pages/Login/LoginForm.tsx
+++ src/pages/Login/LoginForm.tsx
@@ -1,13 +1,7 @@
 import React from 'react'
 
 type LoginProps = {
-  onSubmit: ({
-    username,
-    password,
-  }: {
-    username: string
-    password: string
-  }) => void
+  onSubmit: ({ username, password }: { username: string; password: string }) => void
 }
 
 const LoginForm = ({ onSubmit }: LoginProps) => {
@@ -22,21 +16,11 @@ const LoginForm = ({ onSubmit }: LoginProps) => {
     <div id="login-form">
       <div>
         <label htmlFor="username-field">Username</label>
-        <input
-          id="username-field"
-          type="text"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-        />
+        <input id="username-field" type="text" value={username} onChange={(event) => setUsername(event.target.value)} />
       </div>
       <div>
         <label htmlFor="password-field">Password</label>
-        <input
-          id="password-field"
-          type="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
+        <input id="password-field" type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
       </div>
       <div>
         <button onClick={handleSubmit}>Submit</button>
--- src/utils/test/server-handlers.ts
+++ src/utils/test/server-handlers.ts
@@ -1,29 +1,14 @@
 import { rest, RestRequest } from 'msw'
 
 export const handlers = [
-  rest.post(
-    'https://abc.example.com/api/login',
-    async (
-      req: RestRequest<{ username: string; password: string }>,
-      res,
-      ctx
-    ) => {
-      if (!req.body?.password) {
-        return res(
-          ctx.delay(0),
-          ctx.status(400),
-          ctx.json({ message: 'Password required' })
-        )
-      }
-      if (!req.body?.username) {
-        return res(
-          ctx.delay(0),
-          ctx.status(400),
-          ctx.json({ message: 'Username required' })
-        )
-      }
-
-      return res(ctx.delay(0), ctx.json({ username: req.body.username }))
+  rest.post('https://abc.example.com/api/login', async (req: RestRequest<{ username: string; password: string }>, res, ctx) => {
+    if (!req.body?.password) {
+      return res(ctx.delay(0), ctx.status(400), ctx.json({ message: 'Password required' }))
     }
-  ),
+    if (!req.body?.username) {
+      return res(ctx.delay(0), ctx.status(400), ctx.json({ message: 'Username required' }))
+    }
+
+    return res(ctx.delay(0), ctx.json({ username: req.body.username }))
+  }),
 ]
